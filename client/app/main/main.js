4f730d1eec76ec518f7f1f1415afe7a6cb10a6c3
Nikolai Kiparenko <kiparenkomb@gmail.com>
2016-06-06 20:19:39 +0300

    removing unnecessary files

--- client/app/main/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('itMentorApp')
-  .config(function($stateProvider) {
-    $stateProvider.state('main', {
-      url: '/',
-      template: '<main></main>'
-    });
-  });
--- client/components/auth/auth.module.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('itMentorApp.auth', ['itMentorApp.constants', 'itMentorApp.util', 'ngCookies',
-    'ui.router'
-  ])
-  .config(function($httpProvider) {
-    $httpProvider.interceptors.push('authInterceptor');
-  });
